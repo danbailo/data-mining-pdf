7fc4b929293470a75ff0ef0f080ecdcf431e4bb4
Daniel Bailo <danbailoufms@gmail.com>
2019-10-11 12:13:15 -0400

    tive progresso para reparar os dados ate o ultimo valor da saude

--- script.py
+++ script.py
@@ -2,8 +2,8 @@ import textract
 import re
 from functools import reduce
 
-# text = textract.process("exemplo_extrarcao_pdf.pdf")
-text = textract.process("opcional + 1 titu. 4 a 29 nacional hosp_1.pdf")
+text = textract.process("exemplo_extrarcao_pdf.pdf")
+# text = textract.process("opcional + 1 titu. 4 a 29 nacional hosp_1.pdf")
 
 text = text.decode("utf-8").split("\n")
 
@@ -14,12 +14,17 @@ cifrao_pattern = re.compile(r"(R\$$)")
 adiante_pattern = re.compile(r"(adiante)")
 valor_pattern = re.compile(r"R\$.*")
 
+
 for i in text:
     if i == '': text.remove(i)
-for i in text:
-    if cifrao_pattern.match(i): text.remove(i)
-for i in text:
-    if adiante_pattern.match(i): text.remove(i)             
+# print(text)
+# for i in text:
+#     if cifrao_pattern.match(i): text.remove(i)
+# for i in text:
+#     if adiante_pattern.match(i): text.remove(i)       
+
+
+# exit()
 
 planos = []
 regioes = []
@@ -32,13 +37,28 @@ for i in range(len(text)):
     if regiao_pattern.match(text[i]):
         regioes.append(regiao_pattern.match(text[i])[1])
     if text[i]=="anos":
-        j = i+1
+        j = i
         while True:
-            if text[j]=="Total":
-                all_values = text[i+1:j].copy()
+            if re.match(pattern=r".*(adiante?)",string=text[j]):
+                all_values = text[i+1:j+2].copy()
 
-                value = [value for value in all_values if re.match(pattern=r"(^R\$ ?.+\d)", string=value)]
-                print(value)
+                print(all_values)
+
+                exit()
+
+                all_values_cipher = [value for value in all_values if re.match(pattern=r"(^R\$ ?.+\d)", string=value)]
+            
+                joined_values = " ".join(all_values_cipher)
+
+                for number in joined_values.split():
+                    if re.match(pattern=r"(\d+\,\d+)|(\d\.\d+\,\d+)", string=number):
+                        print(number)
+
+                exit()
+                # test = [cipher.split() for cipher in all_values_cipher]
+
+                # print([t[1] for t in test])
+
                 break # para o primeiro laco
             j += 1
 
