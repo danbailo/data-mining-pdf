85dbee7012ee92999909b7409fc98dcbbf6d8859
Daniel Bailo <danbailoufms@gmail.com>
2019-10-11 17:03:44 -0400

    testando elifs

--- script.py
+++ script.py
@@ -18,39 +18,40 @@ if __name__ == "__main__":
         plano_pattern = re.compile(r"Plano: (.*)")
         regiao_pattern = re.compile(r"Região: (.*)")
         cifrao_pattern = re.compile(r"(R\$$)")
-        adiante_pattern = re.compile(r"(adiante)")
-        valor_pattern = re.compile(r"R\$.*")
+
+        saude_pattern_1 = re.compile(r"(.*anos)")
+        saude_pattern_1 = re.compile(r"(\d\d a \d\d)")
+        saude_pattern_1 = re.compile(r"(.*adiante)")
 
         for i in text:
             if i == '': text.remove(i)
 
         planos = []
         regioes = []
-        teste = []
+        saude = []
         saude_valores = []
         for i in range(len(text)):  
             if calculo_pattern.match(text[i]):
                 calculo = text[i+1]
-            if plano_pattern.match(text[i]):
+            elif plano_pattern.match(text[i]):
                 planos.append(plano_pattern.match(text[i])[1])
-            if regiao_pattern.match(text[i]):
+            elif regiao_pattern.match(text[i]):
                 regioes.append(regiao_pattern.match(text[i])[1])
-            if re.match(pattern=r"(.*anos)", string=text[i]):
+            elif re.match(pattern=r"(.*anos)", string=text[i]):
                 saude_valores.append(text[i+1])
-            if re.match(pattern=r"(\d\d a \d\d)", string=text[i]):
+            elif re.match(pattern=r"(\d\d a \d\d)", string=text[i]):
                 saude_valores.append(text[i+1])
-            if re.match(pattern=r"(.*adiante)", string=text[i]):
+            elif re.match(pattern=r"(.*adiante)", string=text[i]):
                 saude_valores.append(text[i+1])  
-
-            if len(saude_valores) != 0 and len(saude_valores)%10 == 0:            
-                if saude_valores not in teste:
-                    teste.append(saude_valores.copy())
+            elif len(saude_valores) != 0 and len(saude_valores)%10 == 0:   
+                if saude_valores not in saude:
+                    saude.append(saude_valores.copy())
                     saude_valores.clear()
-        print(teste)
-        # print(f"Nome do arquivo: {pdf}\n")
-        # print(f"Cálculo a ser armazenado: {calculo}")
-        # print(f"Plano {planos}")
-        # print(f"Região: {regioes}")
-        # print(f"Saúde(R$): {saude_valores}\n")
-        # print("="*100)
-        # print()
+        # print(saude)
+        print(f"Nome do arquivo: {pdf}\n")
+        print(f"Cálculo a ser armazenado: {calculo}")
+        print(f"Plano {planos}")
+        print(f"Região: {regioes}")
+        print(f"Saúde(R$): {saude_valores}\n")
+        print("="*100)
+        print()
