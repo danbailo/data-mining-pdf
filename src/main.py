e75255d5ad664982fa26916b368d05e62a0eb542
Daniel Bailo <danbailoufms@gmail.com>
2019-10-12 19:23:07 -0400

    updated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+import sys
 from core import Pdf
 
 if __name__ == "__main__":
-    pdf = Pdf()
+
+    if len(sys.argv) != 2:
+        print("USAGE")
+        exit(-1)
+
+    file = sys.argv[1]
+
+    pdf = Pdf(file)
     pdf.get_result()
