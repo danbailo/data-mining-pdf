4293f311969144a9a26c3d847bee993bb09942c7
Daniel Bailo <danbailoufms@gmail.com>
2019-10-14 17:08:53 -0400

    updated

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ def get_args():
 	parser=argparse.ArgumentParser()
 	parser.add_argument("-f","--file",
 		type=str,
-		help="Caminho do PDF ou do diretório que contem o(s) PDF(s).",
-        required=True
+		help="Caminho do PDF ou do diretório que contem o(s) PDF(s). Em caso de dúvidas, leia o arquivo README.md",
+        required=True,
 	) 
 	return parser.parse_args()
