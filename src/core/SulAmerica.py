45ae872f28aa30288fd152e2bd1c98a5a9cfd0d1
Daniel Bailo <danbailoufms@gmail.com>
2019-10-16 01:28:22 -0400

    nova classe para os novos pdfs

---
+++ src/core/SulAmerica.py
@@ -0,0 +1,140 @@
+from .Database import Database
+import textract
+import platform
+import re
+import os
+import datetime
+
+SYSTEM = platform.system()
+
+class SulAmerica:
+    def __init__(self, file = None):
+        if file is None: 
+            print("ERRO: Por favor, entre com um arquivo ou um diretório!")
+            exit(-1)
+        if os.path.isfile(file):
+            self.__file = file
+            self.__directory = None  
+        if os.path.isdir(file):
+            self.__directory = file  
+            self.__file = None
+        self.__db = Database()
+        self.__produto_pattern = re.compile(r"(Produto:)")
+        self.__municipio_pattern = re.compile(r"Município do CNPJ da Empresa: (.*)")
+        self.__regiao_pattern = re.compile(r"Região de Precificação: (.*)")
+        self.__contratacao = re.compile(r"Tipo de Contratação: (.*)")
+        self.__enfermaria = re.compile(r"Exato")
+        self.__saude_pattern_1 = re.compile(r"(\d-\d\d)")
+        self.__saude_pattern_2 = re.compile(r"(\d\d-\d\d)")
+        self.__saude_pattern_3 = re.compile(r"(\d\d \+)")
+
+        # self.__money_sign = re.compile(r"R\$")
+        # self.__comma = re.compile(r"\,")       
+        # self.__dot = re.compile(r" \d{1}(\.)")
+
+    def get_directory(self):
+        return self.__directory
+
+    def get_file(self):
+        return self.__file
+
+    def get_pdfs(self):
+        if self.get_directory():
+            return [pdf for pdf in os.listdir(self.get_directory()) if pdf[-4:] =='.pdf']
+        else:
+            if self.get_file()[-4:] != '.pdf':
+                print("ERRO: Por favor, passe um arquivo .pdf!")
+                exit(-1)
+            return [self.get_file()]
+
+    def insert_result(self, calculo, planos, regioes, saudes):
+        for i in range(len(planos)):
+            extracao_pdf = {
+                "extrator": "SulAmérica",
+                "dt_extracao": datetime.datetime.now(),
+                "campo_01": f"{calculo}",
+                "campo_02": f"{planos[i]}",
+                "campo_03": f"{regioes[i]}",
+                "campo_04":"",
+                "campo_05":"",
+                "campo_06":"",
+                "faixa_01": float(f" {saudes[i][0]}"),
+                "faixa_02": float(f" {saudes[i][1]}"),
+                "faixa_03": float(f" {saudes[i][2]}"),
+                "faixa_04": float(f" {saudes[i][3]}"),
+                "faixa_05": float(f" {saudes[i][4]}"),
+                "faixa_06": float(f" {saudes[i][5]}"),
+                "faixa_07": float(f" {saudes[i][6]}"),
+                "faixa_08": float(f" {saudes[i][7]}"),
+                "faixa_09": float(f" {saudes[i][8]}"),
+                "faixa_10": float(f" {saudes[i][9]}"),
+            }  
+            self.__db.insert_into_extracao_pdf(extracao_pdf)
+    
+    def get_result(self):
+        for pdf in self.get_pdfs():
+            if self.get_directory():
+                text = textract.process(os.path.join(self.get_directory(),pdf))
+            else:
+                text = textract.process(self.get_file())
+                if SYSTEM == "Windows": pdf = pdf.split(r"\\")[-1]
+                else: pdf = pdf.split("/")[-1]
+    
+            text_splitted = text.decode("utf-8").replace('\xa0', '\n').split("\n")
+
+            if SYSTEM == "Windows":
+                text_splitted = [re.sub(pattern=r"\r", repl="", string=t) for t in text_splitted]            
+
+            for i in text_splitted:
+                if i == '': text_splitted.remove(i)
+            
+            # print(text_splitted)
+            # exit()
+
+            values = []
+            for i in range(len(text_splitted)): 
+                if self.__produto_pattern.match(text_splitted[i]):
+                    produto = text_splitted[i+1]
+
+                match_municipio = self.__municipio_pattern.match(text_splitted[i])
+                if match_municipio:
+                    municipio = match_municipio[1]
+                    
+                match_regiao = self.__regiao_pattern.match(text_splitted[i])
+                if match_regiao:
+                    regiao = match_regiao[1]
+
+                match_contratacao = self.__contratacao.match(text_splitted[i])
+                if match_contratacao:
+                    contratacao = match_contratacao[1]
+
+                match_enfermaria = self.__enfermaria.match(text_splitted[i])
+                if match_enfermaria:
+                    enfermaria = match_enfermaria[0] + " " + text_splitted[i+3]
+
+                value1 = self.__saude_pattern_1.match(text_splitted[i])
+                if value1:
+                    values.append(text_splitted[i+2])
+
+                value2 = self.__saude_pattern_2.match(text_splitted[i])
+                if value2:
+                    values.append(text_splitted[i+1])
+
+                value3 = self.__saude_pattern_3.match(text_splitted[i])
+                if value3:
+                    values.append(text_splitted[i+2])              
+
+                if len(values) == 10: break
+        try:
+            print("PRODUTO:",produto)
+            print("MUNICIPIO:",municipio)
+            print("REGIAO:",regiao)
+            print("CONTRATACAO:",contratacao)
+            print("ENFERMARIA:",enfermaria)
+            print("VALORES:",values)
+            exit()
+            print(f'Os dados do PDF "{pdf}" foram inseridos no banco de dados com sucesso!')
+            
+        except Exception:
+            print("ERRO: Por favor, certifique-se que este .pdf contem o mesmo que os outros padrão!")
+            exit(-1)
