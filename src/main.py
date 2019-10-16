04c9e2788e5dbe432db699a01241168046a600c0
Daniel Bailo <danbailoufms@gmail.com>
2019-10-16 01:28:35 -0400

    modificado para testes da nova classe

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from core import Pdf,Database
+from core import Bradesco,SulAmerica,Database
 from utils import get_args
 
 if __name__ == "__main__":
     args = get_args()    
-    pdf = Pdf(args.file)
-    pdf.get_result()
+    # bradesco = Bradesco(args.file)
+    # bradesco.get_result()
+
+    sulamerica = SulAmerica(args.file)
+    sulamerica.get_result()
